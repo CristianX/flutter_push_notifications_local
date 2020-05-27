a2f8a9125bf256f14b03dd3e7a4dd2d4607ac64c
CristianX <thecristianx@hotmail.com>
2020-05-26 20:41:27 -0500

    Configurando utilidades de firebase messaging y enviando notificaciones de prueba desde cloud messaging

--- lib/src/providers/push_notifications_provider.dart
+++ lib/src/providers/push_notifications_provider.dart
@@ -15,7 +15,46 @@ class PushNotificationProvider {
       print('========== FCM Token ==============');
       print(token);
 
+      // e_Ex8rOtadw:APA91bGjkaegyCCSlrL4WMPcw25SPu9J9diRs4QvcOfxZHreUVzLNP3GrE4UM7JADAj3fN1_1HOmEnJoQd2IldDTuCrBJhJo2Jx7HLdpIq1e4YLFSv0o0BUTM6r_fAknN8tgxviAF3LQ
+      
+
     });
+
+    // Configuración de los métodos de firebase messaging
+    _firebaseMessaging.configure(
+
+      // Se dispara cuando la aplicación está abierta
+      onMessage: ( info ) async {
+
+        print( '======= On Message ========' );
+        print( info );
+
+      },
+
+      // Recibir notificaciones
+      onLaunch: ( info ) async {
+
+        print( '======= On Launch ========' );
+        print( info );
+
+        // final notif = info['data']['comida'];
+        // print( notif );
+
+      },
+
+      // Cuando la aplicación se termino
+      onResume: ( info ) async {
+
+        print( '======= On Launch ========' );
+        print( info );
+
+        // final notif = info['data']['comida'];
+        // print( notif );
+
+
+      }
+
+    );
 
   }
 
